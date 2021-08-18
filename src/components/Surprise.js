bb4b65f67dfe64c28852ad9a6ff4bd55b960f5e7
Michael Lackey <michael.g.lackey@gmail.com>
2021-08-18 11:06:42 -0500

    fix surprise

--- src/components/Surprise.js
+++ src/components/Surprise.js
@@ -7,16 +7,16 @@ const Surprise = () => {
       console.log('Unmounted => Clean up any side effects')
     }
   }, [])
-  
+
   return (
     <iframe
-      title="something"
-      width="100%"
-      height="100%"
-      src="https://www.youtube.com/embed/oHg5SJYRHA0?rel=0;&autoplay=1&mute=0&loop=1"
-      frameBorder="0"
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+      title="YouTube video player"
+      frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
+      allowfullscreen
     ></iframe>
   )
 }
